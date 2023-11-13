34851c81722c15c2dc89d27d3d610dd33a3e81cd
adilmangla8 <adil.ahmad@ucp.edu.pk>
2023-11-13 01:14:19 +0000

    added progressBar

--- js/form.js
+++ js/form.js
@@ -1,32 +1,21 @@
-const data = [
-  {
-    id: 1,
-    correctAnswer: "Dharma",
-    incorrectAnswers: ["Dharma", "Nirvana", "Samsara"],
-    question: "From the Sanskrit word for 'duty', what word refers to the teachings of the Buddha?",
-  },
-  {
-    id: 2,
-    correctAnswer: "Friedrich Schiller",
-    incorrectAnswers: ["Sebastian Brant", "Friedrich Schiller", "Rainer Maria Rilke"],
-    question: "Which author wrote 'William Tell'?",
-  },
-  {
-    id: 3,
-    correctAnswer: "Apple",
-    incorrectAnswers: ["Orange", "Apple", "Banana"],
-    question: "What Type Of Fruit Is A Russet?",
-  },
-];
+let data = [];
 
-const shuffleQuestions = () => {
+try {
+  fetch("https://opentdb.com/api.php?amount=10")
+    .then((response) => response.json())
+    .then((questions) => (data = questions.results));
+} catch (error) {
+  console.log(error);
+}
+
+const shuffleQuestions = (data) => {
   for (let i = 0; i < data.length; i++) {
     const j = Math.floor(Math.random() * (i + 1));
     [data[i], data[j]] = [data[j], data[i]];
   }
 };
 
-shuffleQuestions();
+shuffleQuestions(data);
 
 let userAnswersArr = [];
 const timeDisplay = document.getElementById("timer");
@@ -74,17 +63,34 @@ const startCountdown = () => {
 
 function startQuiz() {
   startCountdown();
+  document.getElementById("progressBar").innerHTML = progressBar();
   displayQuestion();
   startBtn.style.display = "none";
 }
 
 startBtn.addEventListener("click", startQuiz);
 
+function progressBar() {
+  if (currentQuestionIndex < data.length) {
+    return `<div class="progress border-0 rounded-0">
+    <div class="progress-bar  bg-warning progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:${
+      100 - (currentQuestionIndex / data.length) * 100
+    }%"></div>
+  </div>`;
+  } else {
+    return `<div></div>`;
+  }
+}
+
 // To Render Data and Update
 const displayQuestion = () => {
   const currentQuestion = data[currentQuestionIndex];
 
-  const options = currentQuestion.incorrectAnswers
+  currentQuestion.incorrect_answers.push(currentQuestion.correct_answer);
+
+  shuffleQuestions(currentQuestion.incorrect_answers);
+
+  const options = currentQuestion.incorrect_answers
     .map((option) => {
       return `<div class='form-check mb-3 p-0'>
     <input class='form-check-input' type='radio' name='answers' id=${option} value="${option}">
@@ -93,9 +99,12 @@ const displayQuestion = () => {
     })
     .join("");
 
+  document.getElementById("progressBar").innerHTML = progressBar();
+
   totalQuestions.innerHTML = `${currentQuestionIndex + 1} / ${data.length}`;
 
-  quizContainer.innerHTML = `<div class="quiz-container">
+  quizContainer.innerHTML = `
+  <div class="quiz-container">
     <h2 class='mb-4'>${currentQuestion.question}</h2>
     ${options}
     <button class='btn btn-primary bg-white text-black border-0 w-100 mt-4 fw-medium' id="nextButton" onclick="checkAnswer()"> Next </button>
@@ -104,22 +113,25 @@ const displayQuestion = () => {
 };
 
 const checkAnswer = () => {
-  console.log("next ");
   const selectedAnswer = document.querySelector("input[type='radio']:checked");
   if (!selectedAnswer) {
-    answerDisplay.innerHTML = "<h3 class='text-center  incorrect'> Please Select An Answer! </h3>";
+    answerDisplay.innerHTML = `<h4 class='font-medium text-center'>Please Select an option</h4>`;
+    answerDisplay.style.color = "#fb6464";
     return;
   }
+
   const currentQuestion = data[currentQuestionIndex];
 
   userAnswersArr.push({ question: currentQuestion.question, userAnswer: selectedAnswer.value, status: "" });
 
-  if (selectedAnswer.value === currentQuestion.correctAnswer) {
-    answerDisplay.innerHTML = `<h3 class='text-center correct'> Correct </h3>`;
+  if (selectedAnswer.value === currentQuestion.correct_answer) {
+    answerDisplay.innerHTML = `<h4 class='font-medium text-center'>Correct</h4>`;
+    answerDisplay.style.color = "#4eff4e";
     userAnswersArr[currentQuestionIndex].status = "correct";
     score++;
   } else {
-    answerDisplay.innerHTML = `<h3 class='text-center  incorrect'> Incorrect </h3>`;
+    answerDisplay.innerHTML = `<h4 class='font-medium text-center'>Incorrect</h4>`;
+    answerDisplay.style.color = "#fb6464";
     userAnswersArr[currentQuestionIndex].status = "Incorrect";
   }
 
@@ -140,6 +152,8 @@ function percentage() {
 }
 
 const displayResult = () => {
+  document.getElementById("progressBar").innerHTML = progressBar();
+
   quizContainer.innerHTML = `
   <div class="quiz-container">
     <h2 class='text-white'>Quiz Ended</h2>
@@ -179,6 +193,6 @@ const resetQuiz = () => {
   timeDisplay.style.display = "block";
   totalQuestions.style.display = "block";
   answerDisplay.innerText = "";
-  shuffleQuestions();
+  shuffleQuestions(data);
   startQuiz();
 };
